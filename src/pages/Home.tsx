6e88327f03b34111d3a19873d1d3247bfe0f800b
rebeca.cid <rebeca.cid@amodosoluciones.com>
2020-02-14 13:55:11 +0100

    Primerito

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { carSportSharp } from 'ionicons/icons';
+import { desktop } from 'ionicons/icons';
+import { airplaneOutline } from 'ionicons/icons';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  let lista = [
+    { name: 'Coches', icon: carSportSharp, class: 'day' },
+    { name: 'Computers', icon: desktop, class: 'day light-scheme' },
+    { name: 'Planes', icon: airplaneOutline, class: 'day dark-scheme' },
+  ];
+  // <ion-icon name="car-sport-outline"></ion-icon>
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -17,6 +26,16 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonList>
+          {
+            lista.map((val, index) => (
+              <IonItem class={val.class}>
+                <IonLabel>{val.name}</IonLabel>
+                <IonIcon icon={val.icon} size="large"></IonIcon>
+              </IonItem>
+            ))
+          }
+        </IonList>
         <ExploreContainer />
       </IonContent>
     </IonPage>
